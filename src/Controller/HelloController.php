8a54d54a53257199b29e800a5f2cc3631ecfe275
afaizulh <ijulhakim14@gmail.com>
2023-02-15 09:03:19 +0700

    first commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ModernFramework\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController
+{
+    public function landing($nama)
+    {
+        return new Response("Hello! Ini merupakan laman utama dari website. Selamat datang $nama!");
+    }
+    public function hello()
+    {
+        return new Response('Hello World');
+    }
+    public function greet($nama)
+    {
+        return new Response(sprintf('Selamat Datang, %s', $nama));
+    }
+}
